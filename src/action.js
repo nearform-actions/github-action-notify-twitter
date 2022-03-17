567f318a4bfdc1d77d413be7447cde88a7afe6ea
Radomir Drndarski <radomir.drndarski@nearform.com>
2022-03-17 11:46:53 +0100

    chore: updated docs and added test cases for missing/invalid inputs

--- src/action.js
+++ src/action.js
@@ -6,25 +6,46 @@ export async function run() {
   core.info(`
   *** ACTION RUN - START ***
   `)
+  const MAX_MESSAGE_LENGTH = 280
+  const message = core.getInput('message')
+  const appKey = core.getInput('twitter-app-key')
+  const appSecret = core.getInput('twitter-app-secret')
+  const accessToken = core.getInput('twitter-access-token')
+  const accessSecret = core.getInput('twitter-access-token-secret')
+
+  if (!message || !appKey || !appSecret || !accessToken || !accessSecret) {
+    core.setFailed('Missing inputs parameters. Please provide all of the following inputs: "message", "twitter-app-key", "twitter-app-secret", "twitter-access-token", and "twitter-access-token-secret"')
+    core.info(`
+    *** ACTION RUN - END ***
+    `)
+    return
+  }
+
+  if (message.length > MAX_MESSAGE_LENGTH) {
+    core.setFailed('The message is too long. The message may contain up to 280 characters.')
+    core.info(`
+    *** ACTION RUN - END ***
+    `)
+    return
+  }
 
   const client = new TwitterApi({
-    appKey: core.getInput('twitter-app-key'),
-    appSecret: core.getInput('twitter-app-secret'),
-    accessToken: core.getInput('twitter-access-token'),
-    accessSecret: core.getInput('twitter-access-token-secret')
+    appKey,
+    appSecret,
+    accessToken,
+    accessSecret
   })
 
   const rwClient = client.readWrite
 
   try {
-    const message = core.getInput('message')
     core.info(`Twitter message: ${message}`)
     await rwClient.v2.tweet(message)
   } catch (err) {
     core.setFailed(`Action failed with error. ${err}`)
   } finally {
     core.info(`
-    *** ACTION RUN - END ***
-    `)
+      *** ACTION RUN - END ***
+      `)
   }
 }
